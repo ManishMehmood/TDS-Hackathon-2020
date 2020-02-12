8c372eebca717f5ff17a88205a2182fe07000480
Manish Mehmood <m.mehmood@contentbloom.com>
2020-02-12 13:21:07 +0530

    Code cleanup

--- AlexaSkill/Helper/Helper.cs
+++ AlexaSkill/Helper/Helper.cs
@@ -10,7 +10,7 @@ namespace AlexaSkill.Helper
     public static class Helper
     {
         private static SessionAwareCoreServiceClient _client;
-
+       
         public static SessionAwareCoreServiceClient Client
         {
             get
@@ -27,6 +27,7 @@ namespace AlexaSkill.Helper
 
                     string username = ConfigurationManager.AppSettings["TridionUsername"];
                     string password = ConfigurationManager.AppSettings["TridionPassword"];
+                    
 
                     if (!String.IsNullOrEmpty(username) && !String.IsNullOrEmpty(password))
                     {
@@ -41,14 +42,11 @@ namespace AlexaSkill.Helper
         {
             try
             {
-          //   string id=GetAllPages().Any(x=>x.Id).Where(x=>x.Title==pageTitle)
-             //   PageData page = (PageData)Client.Read(id, new ReadOptions());
              string id = GetAllPages().Where(x => x != null && x.Title.ToLower().Contains(pageTitle.ToLower())).Select(x=>x.Id).ToList()[0];
-              //  Console.WriteLine("Page title: " + page.Title);
-
                 // Publish the page
                 string[] pageUris = { id };
-                string[] destinationTargetUris = { "tcm:0-11-65538" };
+                string destinationTarget = ConfigurationManager.AppSettings["PublishTargetIdStaging"];
+                string[] destinationTargetUris = { destinationTarget };
                 var publishInstruction = new PublishInstructionData
                 {
                     RenderInstruction = new RenderInstructionData(),
@@ -71,22 +69,14 @@ namespace AlexaSkill.Helper
         }
         public static IdentifiableObjectData[] GetAllPages()
         {
-           // IdentifiableObjectData[] result = null;
+            string rootSGId = ConfigurationManager.AppSettings["RootStructureGroupId"];
             return Client.GetList(
-                "tcm:24-561-4",
+                rootSGId,
                 new OrganizationalItemItemsFilterData
                 {
                     ItemTypes = new[] {ItemType.Page },
                     Recursive = false,
                 });
-          //  if (tridionItems == null)
-           //     return result;
-            //foreach(var page in tridionItems)
-            //{
-            //    result = string.Join(page.Title,",");
-            //}
-           //  result = string.Join(",", tridionItems.Select(x => x.Title).ToArray());
-         //   return result;
         }
     }
 }
